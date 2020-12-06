71519e75bad56151d3c27ec42356d7649604fe2f
Kalai2804 <75132954+Kalai2804@users.noreply.github.com>
2020-12-06 21:52:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Components/Person';
+
+function App(){
+    return(
+        <div className='App'>
+            <h1>Hello Best Enlist!</h1>
+            <Person/>
+
+        </div>
+    )
+}
+export default App;
